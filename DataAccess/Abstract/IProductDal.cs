fdd467b7b76bc67c35ed4670f69ec865e778d758
sinan.ici <sinan.ici@215-03>
2021-12-01 10:06:20 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,19 @@
+﻿using Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        public void Add(Product product);
+        public void Update(Product product);
+        public void Delete(int Id);
+
+        List<Product> GetAllByCategory(int categoryID);
+        List<Product> GetAll();
+    }
+}
